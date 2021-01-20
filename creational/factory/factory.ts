9da9ad543b52de197449e78f3e4ac32f0d5794e0
Alexander Andriets <Alexander.Andriets@acquia.com>
2021-01-20 13:04:34 +0200

    change factory method

--- creational/factory/factory.ts
+++ creational/factory/factory.ts
@@ -1,58 +1,75 @@
-interface Product {
-  operation(): string;
-}
+abstract class Computer {
 
-class Concrete implements Product {
+  protected constructor(public ram: string, public hdd: string, public cpu: string) {
+  }
 
-  public operation(): string {
-    return '{Result of the Concrete}';
+  public abstract getRAM(): string;
+
+  public abstract getHDD(): string;
+
+  public abstract getCPU(): string;
+
+  public getComputerInfo(): string {
+    return `RAM= ${this.getRAM()}, HDD= ${this.getHDD()}, CPU=${this.getCPU()}`;
   }
 
 }
 
-class Brick implements Product {
+class PC extends Computer {
 
-  public operation(): string {
-    return '{Result of the Brick}';
+  constructor(ram: string, hdd: string, cpu: string) {
+    super(ram, hdd, cpu);
+  }
+
+  getCPU(): string {
+    return this.cpu;
+  }
+
+  getHDD(): string {
+    return this.hdd;
+  }
+
+  getRAM(): string {
+    return this.ram;
   }
 
 }
 
-abstract class Creator {
+class Server extends Computer {
 
-  public abstract factoryMethod(): Product;
+  constructor(ram: string, hdd: string, cpu: string) {
+    super(ram, hdd, cpu);
+  }
 
-  public someOperation(): string {
-    const product = this.factoryMethod();
-    return `Creator: The same creator's code has just worked with ${product.operation()}`;
+  getCPU(): string {
+    return this.cpu;
+  }
+
+  getHDD(): string {
+    return this.hdd;
+  }
+
+  getRAM(): string {
+    return this.ram;
   }
 
 }
 
-class ConcreteCreator extends Creator {
+class ComputerFactory {
 
-  public factoryMethod(): Product {
-    return new Concrete();
+  public static getComputer(type: string, ram: string, hdd: string, cpu: string): Computer {
+
+    if (type === 'PC') {
+      return new PC(ram, hdd, cpu);
+    } else if (type === 'Server') {
+      return new Server(ram, hdd, cpu);
+    } else {
+      return null;
+    }
+
   }
 
 }
 
-class BrickCreator extends Creator {
-
-  public factoryMethod(): Product {
-    return new Brick();
-  }
-
-}
-
-function testCode(creator: Creator) {
-  console.log('Client: I\'m not aware of the creator\'s class, but it still works.');
-  console.log(creator.someOperation());
-}
-
-console.log('## Test Start ##');
-
-testCode(new BrickCreator());
-testCode(new ConcreteCreator());
-
-console.log('## Test End ##');
+console.log(ComputerFactory.getComputer('PC', '8', '120', '2').getComputerInfo());
+console.log(ComputerFactory.getComputer('Server', '64', '300', '10').getComputerInfo());
